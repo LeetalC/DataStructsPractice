d7f7d8f12152588730cc3329ff7517e745ab3615
LeetalC <leetalrc@gmail.com>
2019-05-23 17:03:33 -0700

    Made cycle graph

--- DataStructsPractice/Source.cpp
+++ DataStructsPractice/Source.cpp
@@ -9,6 +9,13 @@ typedef int Vertex;
 typedef set<Vertex> VertexSet;
 typedef map<Vertex, VertexSet> Graph2;
 
+
+void maxHeapify(int arr[], int k, int size); //where k is the value up until which the data is not sorted.
+void minHeapify(int arr[]);
+
+
+Graph2 makeCycle(unsigned v);
+void show(const Graph2 &g);
 bool isMinHeap(int arr[]);
 bool isMaxHeap(int arr[]);
 //Graph2 createComp(unsigned v);
@@ -17,7 +24,7 @@ bool isMaxHeap(int arr[]);
 
 int main() {
 	
-	int notheap[7] = { 10,4,78,1,32,34,26 };
+	int notAHeapArr[7] = { 10,4,78,1,32,34,26 };
 	int isaMaxArr[7] = { 19,2,12,1,0, 4, 8 };
 	int isaMinArr[7] = { 1,2,4,6,7,5,8 };
 
@@ -28,16 +35,19 @@ int main() {
 	case 1:
 		cout << isMinHeap(isaMaxArr) << endl;
 		cout << isMinHeap(isaMinArr) << endl;
-		cout << isMinHeap(notheap) << endl;
+		cout << isMinHeap(notAHeapArr) << endl;
 
 		cout << isMaxHeap(isaMaxArr) << endl;
 		cout << isMaxHeap(isaMinArr) << endl;
-		cout << isMaxHeap(notheap) << endl;
+		cout << isMaxHeap(notAHeapArr) << endl;
 		break;
 	case 2:
-		cout << "hi" << endl;
+		Graph2 g = makeCycle(7);
+		show(g);
+		//maxHeapify(notAHeapArr, (sizeof(notAHeapArr) / sizeof(int) - 1));
+
 	}
-	
+
 	//isheap(isaheap);
 
 	//Graph2 comp = createComp(5);
@@ -63,6 +73,40 @@ bool isMaxHeap(int arr[]) {
 		if (arr[2 * i + 1] > arr[i]) return false;
 	}
 	return true;
+}
+
+//void maxHeapify(int arr[], int k, int size) {
+////a max heap has the property where each node has a child that is smaller than itself. (left/right)
+////the parent is located at k/2, left child is at 2*k and rigth child is at 2*k+1
+//	
+//	int temp;
+//	int parent = size / 2 - 1;
+//	int current = parent;
+//	for(int i = 0; )
+//
+//}
+
+Graph2 makeCycle(unsigned v) {
+	Graph2 g;
+	for (unsigned i = 0; i < v-1; ++i) {
+		g[i].insert(i + 1);
+		g[i + 1].insert(i);
+
+	}
+	g[v - 1].insert(0);
+	g[0].insert(v - 1);
+	return g;
+}
+
+void show(const Graph2 &g) {
+	for (auto itr = g.begin(); itr != g.end(); itr++) {
+		cout << itr->first << endl;
+		for (auto it = itr->second.begin(); it != itr->second.end(); it++) {
+			cout << *it << " ";
+		}
+		cout << endl;
+	}
+
 }
 //Graph2 createComp(unsigned v) {
 //	Graph2 g;
