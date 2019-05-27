a42f7e0efafa164865d764340defc6b2b6f20921
LeetalC <leetalrc@gmail.com>
2019-05-27 01:12:59 -0700

    isWheel works and is small
    A lot of error checking, all functions working and line efficient

--- DataStructsPractice/Source.cpp
+++ DataStructsPractice/Source.cpp
@@ -18,39 +18,47 @@ void show(const Graph2 &g);
 
 int main() {
 
-	int c;
+	int c, numErrs = 0;
+	//premade wheel
+	Graph2 test{ {0,{1,4,5}}, {1,{0,2,5}}, {2,{1,3,5}}, {3,{2,4,5}}, {4,{0,3,5}}, {5,{0,1,2,3,4}}};
+    cout << "Is this a wheel?: " << isWheel(test) << endl << endl;
+	cout << "------------------------------------------------------------" << endl;
 	cout << "Which graph do you want to test?" << endl;
 	cout << "1 for Wheel, 2 for Complete, 3 for Cycle." << endl;
+
 
 	while (cin >> c) {
 		switch (c) {
 		case 1:
-			for (int i = 0; i < 5; i++) {
-				int s = rand() % 5 + 4;
+			for (int i = 0; i < 200; i++) {
+				int s = rand() % 100 + 4;
 				Graph2 whee = makeWheel(s);
-				show(whee);
-				cout << "Is whee a wheel?: " << isWheel(whee) << endl;
+				//show(whee);
+				if (!(isWheel(whee))) numErrs++;
 			}
+			cout << "ERRORS FOR WHEEL: " << numErrs << endl;
 			break;
 		case 2:
-			for (int i = 0; i < 5; i++) {
-				int s = rand() % 5 + 1;
+			for (int i = 0; i < 200; i++) {
+				int s = rand() % 100 + 5;
 				Graph2 comp = makeComplete(s);
-				show(comp);
-				cout << "Is complete a wheel?: " << isWheel(comp) << endl;
+				//show(comp);
+				if (isWheel(comp)) numErrs++;
 			}
+			cout << "ERRORS FOUND FOR COMPLETE: " << numErrs << endl;
 			break;
 		case 3:
-			for (int i = 0; i < 25; i++) {
-				int s = rand() % 30 + 4;
+			for (int i = 0; i < 200; i++) {
+				int s = rand() % 100 + 4;
 				Graph2 cyc = makeCycle(s);
-				show(cyc);
-				cout << "Is cyc a wheel?: " << isWheel(cyc) << endl;
-				cout << endl;
+				//show(cyc);
+				if (isWheel(cyc)) numErrs++;
 			}
+			cout << "ERRORS FOUND FOR CYCLE: " << numErrs << endl;
 			break;
 		default: cout << "fucked" << endl;
 		}
+		numErrs = 0;
 	}
 
 	system("pause");
@@ -71,6 +79,7 @@ Graph2 makeCycle(unsigned v) {
 	return gr;
 	//perfect
 }
+
 Graph2 makeWheel(unsigned v) {
 	Graph2 gr = makeCycle(v - 1);
 	for (int i = 0; i < v - 1; ++i) {
@@ -79,12 +88,11 @@ Graph2 makeWheel(unsigned v) {
 
 	}
 	return gr;
-	//perfect
 }
 
 Graph2 makeComplete(unsigned v) {
 	Graph2 g;
-	for(int i = 0; i < v; ++i){
+	for (int i = 0; i < v; ++i) {
 		for (int k = 0; k < v; ++k) {
 			if (k != i) {
 				g[i].insert(k);
@@ -96,33 +104,18 @@ Graph2 makeComplete(unsigned v) {
 }
 
 bool isWheel(const Graph2 &g) {
-	int s = g.size(); //number of verts in the graph
-	int edges = 0; //going to find the number of edges in the graph. a wheel has 2(s-1) edges, soif s = 5, then this graph should have 2*4 edges == 8 edges.
-	auto center = g.begin();
-	bool centerFound = false;
+	int edges = 0;
 
-    //find the vertex with the largest adjacency list, equal to the size of the graph, minus itself
-	//this is also used for obvious removals
-	for (auto max = g.begin(); max != g.end(); max++) {
-		if (max->second.size() < 3) return false;
-		if (max->second.size() == s - 1) {
-			center = max;
-			centerFound = true;
+	for (auto itr = g.begin(); itr != g.end(); itr++) {
+		for (auto neighbor = itr->second.begin(); neighbor != itr->second.end(); neighbor++) {
+			auto it = g.find(*neighbor);
+			if (it->second.find(itr->first) != it->second.end()) edges++;
+			else return false;
 		}
 	}
 
-	if (centerFound) {
-		for (auto itr = g.begin(); itr != g.end(); itr++) {	//for every element in the map (vertex) 
-			for (auto neighbor = itr->second.begin(); neighbor != itr->second.end(); neighbor++) {	//for every element in the set of vertex
-				auto it = g.find(*neighbor); //we start with vertex 0, which has 1,3,and 4 as neighbors. g.find(1) is found so we go to it
-				/*does vertex 1 contain 0 in it's set? if so, add an edge*/
-				if (it->second.find(itr->first) != it->second.end()) edges++; //this will find all existing edges and counts them twice
-				else return false;	//there existed a set without a match, this is not a wheel.
-			}
-		}
-		edges = edges / 2;
-		if (edges == 2 * (s - 1)) return true;
-	}
+	edges = edges / 2;
+	if (edges == 2 * (g.size() - 1)) return true;
 	return false;
 }
 
